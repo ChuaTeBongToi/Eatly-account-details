06fbcf19912d21efa0aeafa57798c21b63d33756
ChuaTeBongToi <hbaoanh2k9@gmail.com>
2023-07-17 15:05:31 +0700

    fix

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-ReactDOM.render(<h1>Hi</h1>, document.getElementById("root"))
+function uploadFile() {
+    document.getElementById("input-file").click();
+}
+
+function previewFile() {
+    var preview = document.getElementById('photo');
+    var file = document.querySelector('input[type=file]').files[0];
+    var reader = new FileReader();
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = "Ellipse 194.png";
+    }
+
+    reader.onloadend = function () {
+        preview.src = reader.result;
+    }
+}
+
+function deleteFile() {
+   document.getElementById('photo').src = "Account Owner.png";
+   file = "Ellipse 194.png";
+}
